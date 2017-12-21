b41c17dea20b8b979465a6e636d9b9a7922450db
Alceu Rodrigues de Freitas Junior <arfreitas@cpan.org>
2017-12-21 14:28:32 -0200

    Fixed warnings from checkstyle

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -8,15 +8,18 @@ public class Greeter {
     /**
      * This is a constructor.
      */
+
     public Greeter() {
 
     }
 
-    /*
-  * @param someone the name of the person
-  * @return greeting string
+    /**
+     *
+     * @param someone
+     * @return
      */
-    final public String greet(final String someone) {
+
+    public final String greet(final String someone) {
         return String.format("Hello, %s!", someone);
     }
 }
